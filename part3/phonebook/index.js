e07527fb9b2a3d9599dc7d47e8f30ef93f193b63
Toni Pellinen <toni.pellinen@hotmail.com>
2020-08-06 21:51:55 +0300

    Parts 3.3-3.6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       "name": "Arto Hellas",
@@ -31,6 +33,49 @@ app.get('/info', (reg, res) => {
 
 app.get('/api/persons', (reg, res) => {
     res.json(persons)
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const person = persons.find(person => person.id === id)
+  console.log(person)
+  if (person) {
+    res.json(person)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+  console.log(id)
+  res.status(202).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const person = req.body
+
+  if (!person.name) {
+    return res.status(400).json({
+      error: 'Name is missing'
+    })
+  } else if (!person.number) {
+    return res.status(400).json({
+      error: 'Number is missing'
+    })
+  } else if (persons.some(p => p.name === person.name)) {
+    return res.status(400).json({
+      error: 'Person is already in phonebook'
+    })
+  }
+
+  const id = Math.floor(Math.random() * Math.floor(10000))
+  person.id = id
+
+  persons = persons.concat(person)
+
+  res.json(person)
 })
 
 const PORT = 3001

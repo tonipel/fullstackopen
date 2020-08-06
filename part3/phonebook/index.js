290897deb8351fced3d709415cec0fe3a1462020
Toni Pellinen <toni.pellinen@hotmail.com>
2020-08-06 22:18:53 +0300

    3.7-3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,44 +1,52 @@
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
 app.use(express.json())
 
+morgan.token('body', function (req, res) {
+  return JSON.stringify(req.body)
+})
+
+app.use(morgan(':method :url :status - :response-time ms :body'))
+
 let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": 1
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": 2
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    }
+  {
+    "name": "Arto Hellas",
+    "number": "040-123456",
+    "id": 1
+  },
+  {
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523",
+    "id": 2
+  },
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  }
 ]
 
 app.get('/info', (reg, res) => {
-    const date = new Date()
-    res.send(`Phonebook has info for ${persons.length} people <br><br> ${date.toString()}`)
+  const date = new Date()
+  res.send(`Phonebook has info for ${persons.length} people <br><br> ${date.toString()}`)
 })
 
 app.get('/api/persons', (reg, res) => {
-    res.json(persons)
+  res.json(persons)
 })
 
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
-  console.log(person)
+
   if (person) {
     res.json(person)
   } else {
@@ -49,7 +57,7 @@ app.get('/api/persons/:id', (req, res) => {
 app.delete('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   persons = persons.filter(person => person.id !== id)
-  console.log(id)
+
   res.status(202).end()
 })
 
@@ -80,5 +88,5 @@ app.post('/api/persons', (req, res) => {
 
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })

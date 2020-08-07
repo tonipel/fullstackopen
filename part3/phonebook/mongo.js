c388130601036e862805188295de9ca00f893302
Toni Pellinen <toni.pellinen@hotmail.com>
2020-08-07 23:26:54 +0300

    3.12

---
+++ part3/phonebook/mongo.js
@@ -0,0 +1,41 @@
+
+const mongoose = require('mongoose')
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack_toni:${password}@cluster0.blsth.mongodb.net/phonebook-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+console.log(process.argv.length)
+if (name === undefined && number === undefined) {
+  
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+
+} else {
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(result => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}

a4cf0dd0edfa4c2b5e0e047c9799a938c011b587
Toni Pellinen <toni.pellinen@hotmail.com>
2020-09-20 15:12:55 +0300

    5.18

--- part5/bloglist-frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist-frontend/cypress/integration/blog_app.spec.js
@@ -1,10 +1,36 @@
 describe('Blog app', function() {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3003/api/testing/reset')
+
+    const user = {
+      name: 'Test User',
+      username: 'testuser',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3003/api/users/', user)
+
     cy.visit('http://localhost:3000')
   })
 
   it('Login form is shown', function() {
     cy.contains('Log in to application')
+  })
+
+  describe('Login',function() {
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('salasana')
+      cy.get('#login-button').click()
+
+      cy.contains('Test User logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('testuser')
+      cy.get('#password').type('wrong password')
+      cy.get('#login-button').click()
+
+      cy.contains('Log in to application')
+    })
   })
 })
--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -151,6 +151,7 @@ const App = () => {
         <div>
           username
           <input
+            id="username"
             type="text"
             value={username}
             name="Username"
@@ -160,13 +161,14 @@ const App = () => {
         <div>
           password
           <input
+            id="password"
             type="password"
             value={password}
             name="Password"
             onChange={({ target }) => setPassword(target.value)}
           />
         </div>
-        <button type="submit">login</button>
+        <button id="login-button" type="submit">login</button>
       </form>
     </div>
   )
